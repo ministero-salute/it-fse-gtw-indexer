bcd45491c96f705f58926998820a23e049ed855b
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2023-03-15 19:11:24 +0100

    Update KafkaHealthIndicator.java

--- src/main/java/it/finanze/sanita/fse2/ms/gtwindexer/config/health/KafkaHealthIndicator.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtwindexer/config/health/KafkaHealthIndicator.java
@@ -10,6 +10,7 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 import it.finanze.sanita.fse2.ms.gtwindexer.config.kafka.KafkaPropertiesCFG;
+import it.finanze.sanita.fse2.ms.gtwindexer.utility.ProfileUtility;
 
 @Component
 public class KafkaHealthIndicator implements HealthIndicator {
@@ -17,10 +18,20 @@ public class KafkaHealthIndicator implements HealthIndicator {
 	@Autowired
     private KafkaPropertiesCFG kafkaCFG;
 
+	@Autowired
+	private ProfileUtility profileUtility;
+	
     @Override
     public Health health() {
     	Properties configProperties = new Properties();
     	configProperties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaCFG.getProducerBootstrapServers());
+    	if(!profileUtility.isDevOrDockerProfile() && !profileUtility.isTestProfile()) {
+    		configProperties.put("security.protocol", kafkaCFG.getProtocol());
+    		configProperties.put("sasl.mechanism", kafkaCFG.getMechanism());
+    		configProperties.put("sasl.jaas.config", kafkaCFG.getConfigJaas());
+    		configProperties.put("ssl.truststore.location", kafkaCFG.getTrustoreLocation());  
+    		configProperties.put("ssl.truststore.password", String.valueOf(kafkaCFG.getTrustorePassword())); 
+		}
         try(AdminClient adminClient = AdminClient.create(configProperties)) {
             adminClient.listTopics().listings().get();
             return Health.up().build();
