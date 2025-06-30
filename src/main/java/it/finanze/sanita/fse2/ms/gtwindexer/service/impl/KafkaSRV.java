579c3291c5daa92e96ae8dcae555c38caac10b9c
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-06-30 11:52:34 +0200

    feat: Remove b3 extractor and stop flow when ini is mocked

--- src/main/java/it/finanze/sanita/fse2/ms/gtwindexer/service/impl/KafkaSRV.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtwindexer/service/impl/KafkaSRV.java
@@ -11,7 +11,28 @@
  */
 package it.finanze.sanita.fse2.ms.gtwindexer.service.impl;
 
+import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventStatusEnum.BLOCKING_ERROR;
+import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventStatusEnum.BLOCKING_ERROR_MAX_RETRY;
+import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventStatusEnum.SUCCESS;
+import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventTypeEnum.DESERIALIZE;
+import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventTypeEnum.SEND_TO_INI;
+import static it.finanze.sanita.fse2.ms.gtwindexer.utility.StringUtility.toJSONJackson;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
 import com.google.gson.Gson;
+
 import it.finanze.sanita.fse2.ms.gtwindexer.client.IIniClient;
 import it.finanze.sanita.fse2.ms.gtwindexer.client.base.ClientCallback;
 import it.finanze.sanita.fse2.ms.gtwindexer.config.AccreditationSimulationCFG;
@@ -22,7 +43,11 @@ import it.finanze.sanita.fse2.ms.gtwindexer.dto.request.IndexerValueDTO;
 import it.finanze.sanita.fse2.ms.gtwindexer.dto.request.IniDeleteRequestDTO;
 import it.finanze.sanita.fse2.ms.gtwindexer.dto.request.IniMetadataUpdateReqDTO;
 import it.finanze.sanita.fse2.ms.gtwindexer.dto.response.IniTraceResponseDTO;
-import it.finanze.sanita.fse2.ms.gtwindexer.enums.*;
+import it.finanze.sanita.fse2.ms.gtwindexer.enums.EventStatusEnum;
+import it.finanze.sanita.fse2.ms.gtwindexer.enums.EventTypeEnum;
+import it.finanze.sanita.fse2.ms.gtwindexer.enums.OperationLogEnum;
+import it.finanze.sanita.fse2.ms.gtwindexer.enums.ProcessorOperationEnum;
+import it.finanze.sanita.fse2.ms.gtwindexer.enums.ResultLogEnum;
 import it.finanze.sanita.fse2.ms.gtwindexer.exceptions.BlockingIniException;
 import it.finanze.sanita.fse2.ms.gtwindexer.exceptions.BusinessException;
 import it.finanze.sanita.fse2.ms.gtwindexer.service.IAccreditamentoSimulationSRV;
@@ -30,25 +55,6 @@ import it.finanze.sanita.fse2.ms.gtwindexer.service.IConfigSRV;
 import it.finanze.sanita.fse2.ms.gtwindexer.service.IKafkaSRV;
 import it.finanze.sanita.fse2.ms.gtwindexer.service.KafkaAbstractSRV;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Function;
-
-import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventStatusEnum.*;
-import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventTypeEnum.DESERIALIZE;
-import static it.finanze.sanita.fse2.ms.gtwindexer.enums.EventTypeEnum.SEND_TO_INI;
-import static it.finanze.sanita.fse2.ms.gtwindexer.utility.KafkaUtility.getTraceContext;
-import static it.finanze.sanita.fse2.ms.gtwindexer.utility.StringUtility.toJSONJackson;
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Kafka management service.
@@ -74,6 +80,8 @@ public class KafkaSRV extends KafkaAbstractSRV implements IKafkaSRV {
 	
 	@Autowired
 	private IConfigSRV configSRV;
+	
+	private static final String MOCK_MESSAGE = "Regime di mock abilitato";
 
 	@Override
 	@KafkaListener(topics = "#{'${kafka.dispatcher-indexer.base-topic}'}",
@@ -129,7 +137,8 @@ public class KafkaSRV extends KafkaAbstractSRV implements IKafkaSRV {
 		
 		IniTraceResponseDTO response = sendToIniClient(valueInfo);
 
-		if (Boolean.TRUE.equals(response.getEsito()) && !configSRV.isRemoveEds()) { 
+		if (Boolean.TRUE.equals(response.getEsito()) && !configSRV.isRemoveEds() 
+				&& !MOCK_MESSAGE.equals(response.getMessage())) { 
 			log.debug("Successfully sent data to INI for workflow instance id" + valueInfo.getWorkflowInstanceId() + " with response: true", OperationLogEnum.CALL_INI, ResultLogEnum.OK, startDateOperation);
 			try {
 				sendMessage(destTopic, cr.key(), cr.value());
@@ -157,8 +166,6 @@ public class KafkaSRV extends KafkaAbstractSRV implements IKafkaSRV {
 
 
 	private <T> void loop(ConsumerRecord<String, String> cr,Class<T> clazz,ClientCallback<T, IniTraceResponseDTO> cb,int delivery,Function<T, String> extractor) throws Exception {
-
-		getTraceContext(cr).ifPresent(hd -> log.info("Logging transaction with context {}", new String(hd.value(), UTF_8)));
 
 		// ====================
 		// Deserialize request
